74d6f22b0b81c855e06d08bbdf8cb54f36a73f60
RegnarsP <regnarspinta3@gmail.com>
2024-04-08 23:52:26 +0300

    first commit

---
+++ Lab/src/App.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div>
+      {/* <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" /> */}
+      <Spline scene="https://my.spline.design/SByJ2DAOQEnt7kLuwiM9eXB7/scene.splinecode" />
+    </div>
+  );
+}
